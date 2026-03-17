8d19e0334dfaf7aab41f2372a5b50b71dd31e83a
Harish1685 <kumarharish1680@gmail.com>
2026-03-17 22:48:40 +0530

    modified providers and main

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,3 +13,13 @@ terraform {
   }
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
